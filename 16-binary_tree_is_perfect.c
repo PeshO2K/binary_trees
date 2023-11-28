d8d719e2a436436dbf54b260db50019862d43587
Patience Otuke <patienceotuke@gmail.com>
2023-11-28 21:51:00 +0300

    Task 16 Betty fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -62,19 +62,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	/*if (binary_tree_balance(tree) == 0)
-		return (1);
-	else
-		return (0);
-	return (binary_tree_is_perfect(tree->left) *
-			binary_tree_is_perfect(tree->right) *
-			((tree->left && tree->right)));
-	*/
 	if (binary_tree_balance(tree) != 0)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
 	if (!tree->left || !tree->right)
 		return (0);
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left) &&
+	binary_tree_is_perfect(tree->right));
 }
