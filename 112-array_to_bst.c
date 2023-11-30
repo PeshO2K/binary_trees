b5c1cd4a85bd513eceadb6bddc5c5ee5b716a32a
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-11-30 15:43:27 +0000

    Add 112 with lazy check of failure and no free

---
+++ 112-array_to_bst.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - builds a BST from an array
+ * @array: pointer to first element of the array
+ * @size: number of elements in the array
+ * Return: Pointer to the root node or NULL on failure
+ */
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *root, *tmp, *init = NULL;
+	size_t i = 1;
+
+	if (size > 0)
+	{
+		root = bst_insert(&init, array[0]);
+		if (!root)
+			return (NULL);
+		while (i < size)
+		{
+			tmp = bst_insert(&root, array[i]);
+			if (!tmp)
+				return (NULL);
+			i++;
+		}
+		return (root);
+	}
+	return (NULL);
+}
