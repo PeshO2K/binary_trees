affab8b67d000c77d8b8899c1782bdeaa31caf2f
Patience Otuke <patienceotuke@gmail.com>
2023-12-02 07:57:09 +0300

    Testing 110 alt algo

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -70,6 +70,23 @@ int is_bst_helper(const binary_tree_t *tree)
 	return (1);
 }
 /**
+ * is_bst - helper func for checking bst
+ * @tree: root node
+ * @min: minimum limit
+ * @max: maximum limit
+ * Return: 1 if is bst, 0 otherwise, 1 if NULL root
+ */
+int is_bst(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+	return (is_bst(tree->left, min, tree->n) &&
+	is_bst(tree->right, tree->n, max));
+
+}
+/**
  * binary_tree_is_bst - checks if a BT is a valid BST
  * @tree: pointer to the root node
  * Return: 1 if BST, 0 otherwise, 0 also for NULL root
@@ -78,5 +95,5 @@ int is_bst_helper(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (is_bst_helper(tree));
+	return (is_bst(tree, INT_MIN, INT_MAX));
 }
